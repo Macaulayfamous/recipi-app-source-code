b19b07bdb596f0273f80a07f5c310fef55236186
mac <owhedofamous12345>
2020-12-22 22:44:51 +0000

    Added custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,18 +1,39 @@
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
 
-    def test_create_user_with_email_succesful(self):
-        """test  creating a new user with email its succesful"""
+    def test_create_user_with_email_succecfull(self):
+        """creating user with email instead of username"""
 
-        email = 'mac@gmail.com'
-        password = 'beejaymac@gmail.com'
+        email = "owhedofamous@gmail.com"
+        password = 'beejaymac'
 
         user = get_user_model().objects.create_user(
-            email=email,
-            password=password
+            email = email,
+            password = password
         )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+
+    def test_new_user_email_normalized(self):
+        email = "test@BEEJAYMAC.COM"
+        user = get_user_model().objects.create_user(email, 'beejaymac')
+
+        self.assertEqual(user.email, email.lower())
+    def test_new_user_invalid_email(self):
+         """test creating user with no email will raise and error"""
+         with self.assertRaises(ValueError):
+             get_user_model().objects.create_user(None,'beejaymac')
+    def test_create_new_superuser(self):
+        """test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'beejaymac@gmail', 'beejaymac'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+         
